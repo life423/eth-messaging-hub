c6f03efe1bade937341e6f62529da4864878e00c
Andrew Clark <aiandrew631@gmail.com>
2024-11-12 13:59:47 -0600

    refactor file tree

--- src/index.js
+++ src/index.js
@@ -1,14 +1,13 @@
-const express = require('express');
-const app = express();
-const port = 3000;
+require('dotenv').config()
+const express = require('express')
+const app = express()
 
 app.get('/', (req, res) => {
-  res.send('Hello from eth-messaging-hub!');
-});
+    res.send('Hello from eth-messaging-hub@@!')
+})
 
-
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000
 
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+    console.log(`Server is running on port ${PORT}`)
+})
